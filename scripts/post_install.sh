98ca8ed8aac23b4f9cdf069c6a160b8b321247af
Neclow <neilclow24@gmail.com>
2025-10-28 15:26:23 +0000

    feat: add beast package installation to post_install script

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -10,8 +10,14 @@ uv pip install flash-attn --no-build-isolation
 # Install missing R packages
 Rscript -e 'install.packages("Quartet", repos="https://cloud.r-project.org")'
 
-# Install BEAST 2.7
-wget -O extern/beast2.tgz https://github.com/CompEvol/beast2/releases/download/v2.7.7/BEAST.v2.7.7.Linux.x86.tgz
-tar -xvzf extern/beast2.tgz -C extern/
+# Install BEAST 2.7 and packages
+BEAST_TARGET=extern/beast2.tgz
+BEAST_PKG_DIR=.beast
+wget -O $BEAST_TARGET https://github.com/CompEvol/beast2/releases/download/v2.7.7/BEAST.v2.7.7.Linux.x86.tgz
+tar -xvzf $BEAST_TARGET -C extern/
 mv extern/beast extern/beast2
-
+mkdir -p $BEAST_PKG_DIR
+pixi run packagemanager -add BEASTLabs -version 2.0.2 -dir $BEAST_PKG_DIR
+pixi run packagemanager -add BDSKY -version 1.5.1 -dir $BEAST_PKG_DIR
+pixi run packagemanager -add SA -version 2.1.1 -dir $BEAST_PKG_DIR
+pixi run packagemanager -add CCD -version 1.0.2 -dir $BEAST_PKG_DIR
