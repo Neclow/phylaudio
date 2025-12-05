125e1f6a5781fc496e47d06490f55bfdbe7e196e
Neclow <neilclow24@gmail.com>
2025-12-05 18:33:13 +0000

    refactor: add secure protocol for beast install

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -13,7 +13,7 @@ Rscript -e 'install.packages("Quartet", repos="https://cloud.r-project.org")'
 # Install BEAST 2.7 and packages
 BEAST_TARGET=extern/beast2.tgz
 BEAST_PKG_DIR=.beast
-wget -O $BEAST_TARGET https://github.com/CompEvol/beast2/releases/download/v2.7.7/BEAST.v2.7.7.Linux.x86.tgz
+wget --secure-protocol=auto --max-redirect=3 -O $BEAST_TARGET https://github.com/CompEvol/beast2/releases/download/v2.7.7/BEAST.v2.7.7.Linux.x86.tgz
 tar -xvzf $BEAST_TARGET -C extern/
 mv extern/beast extern/beast2
 mkdir -p $BEAST_PKG_DIR
@@ -21,6 +21,7 @@ pixi run packagemanager -add BEASTLabs -version 2.0.2 -dir $BEAST_PKG_DIR
 pixi run packagemanager -add BDSKY -version 1.5.1 -dir $BEAST_PKG_DIR
 pixi run packagemanager -add SA -version 2.1.1 -dir $BEAST_PKG_DIR
 pixi run packagemanager -add CCD -version 1.0.2 -dir $BEAST_PKG_DIR
+rm $BEAST_TARGET
 
 # Install DensiTree
 wget -O extern/DensiTree.jar https://github.com/rbouckaert/DensiTree/releases/download/v3.0.0/DensiTree.v3.1.0.jar
