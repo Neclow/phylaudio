2e3fa236b42a97156372a532b6e46d8f66432468
Neclow <neilclow24@gmail.com>
2026-01-05 15:56:24 +0000

    refactor: simplify model downloading by enforcing default args

--- pipeline/download_models.py
+++ pipeline/download_models.py
@@ -1,4 +1,3 @@
-import os
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
 from src._config import DEFAULT_CACHE_DIR, SAMPLE_RATE
@@ -6,27 +5,19 @@ from src.models._model_zoo import MODEL_ZOO
 
 
 def parse_args():
-    """Parse arguments for FLEURS-R data preparation"""
+    """Parse arguments for downloading audio models"""
 
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument(
-        "output_dir",
-        type=str,
-        default=DEFAULT_CACHE_DIR,
-        help=f"Output dataset folder. Example: {DEFAULT_CACHE_DIR}",
     )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    os.makedirs(args.output_dir, exist_ok=True)
     for key, model in MODEL_ZOO.items():
-        print(key)
-        print(model)
-        extractor = model["extractor"](model_id=key, cache_dir=args.output_dir)
+        print(f"Downloading model: {key}")
+        extractor = model["extractor"](model_id=key, cache_dir=DEFAULT_CACHE_DIR)
         processor = model["processor"](
-            model_id=key, cache_dir=args.output_dir, sr=SAMPLE_RATE
+            model_id=key, cache_dir=DEFAULT_CACHE_DIR, sr=SAMPLE_RATE
         )
