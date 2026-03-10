0f5d237ffa5ff2f0623e43a331ffeeccbe68fae8
Neclow <neilclow24@gmail.com>
2026-03-10 17:32:20 +0000

    refactor: simplify download_models

--- pipeline/download_models.py
+++ pipeline/download_models.py
@@ -1,20 +1,7 @@
-from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
-
 from src._config import DEFAULT_CACHE_DIR, SAMPLE_RATE
 from src.models._model_zoo import MODEL_ZOO
 
-
-def parse_args():
-    """Parse arguments for downloading audio models"""
-
-    parser = ArgumentParser(
-        formatter_class=ArgumentDefaultsHelpFormatter,
-    )
-    return parser.parse_args()
-
-
 if __name__ == "__main__":
-    args = parse_args()
     for key, model in MODEL_ZOO.items():
         print(f"Downloading model: {key}")
         extractor = model["extractor"](model_id=key, cache_dir=DEFAULT_CACHE_DIR)
